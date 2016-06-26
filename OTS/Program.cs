031b1d8fd87a892dfb84a491dafc40daf38ffd06
Hua Zhang <hua.zhang@wsu.edu>
2016-06-26 11:23:09 -0700

    update test cases.

--- OTS/Program.cs
+++ OTS/Program.cs
@@ -6,14 +6,20 @@ namespace OTS
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{
-			string name = "Hua"; 
-			Person[] acquaintance =  {new Person("bob", new Person[] {new Person("Alice", new Person[0])})};
+		{// 把 while ( myStack.Count >＝ 0 );   改为 while ( myStack.Count > 0 );
+			Person alice = new Person("Alice", new Person[0]);
+			Person mark = new Person("Mark", new Person[0]);
+			Person hua = new Person("Hua", new Person[] {mark});
+			Person bob = new Person("Bob", new Person[] {alice, mark, hua}) ;
 
+			string name = "Tony";
+			Person[] acquaintance = new Person[] { alice, mark, hua, bob };
 			Person person = new Person( name, acquaintance);
 
-			string target = "bob";
-			Console.WriteLine( person.Mystery( target )); // true 
+			string[] targets = { "Alice", "Mark", "Hua", "Bob" , "Tom"};
+			for (int i = 0; i < targets.Length; i++) {
+				Console.WriteLine( person.Mystery( targets[i] )); // True, True, True, True, False
+			}
 		}
 	}
 }
